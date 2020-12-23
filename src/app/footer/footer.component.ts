f7defeba0d5ac95263192f700623e90cc35334f9
PoriaEbrahimi <poria.ebrahimi123@gmail.com>
2020-12-23 04:12:46 -0600

    Added logos for social media. Added container in footer to hold social media links.

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  socialMediaArray: String[] = [
+    "https://github.com/PoriaEbrahimi",
+    "mailto:poria.ebrahimi123@gmail.com",
+    "https://www.linkedin.com/in/poria-ebrahimi/"
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  randomSocialMedia = () => {
+    var pos = Math.floor(Math.random() * 3);
+    return this.socialMediaArray[pos]
   }
 
 }
