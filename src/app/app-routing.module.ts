bca86e43e28badb4001d65fa9226fc30863b383c
PoriaEbrahimi <poria.ebrahimi123@gmail.com>
2022-05-21 05:17:13 -0500

    Updated app and routing modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './body/home/home.component';
+import { BrowseComponent } from './body/browse/browse.component';
+import { PageNotFoundComponent } from './body/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent },
+  { path: 'browse', component: BrowseComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: false })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
