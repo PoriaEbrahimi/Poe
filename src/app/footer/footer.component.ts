df54d3e92f4a767dbaa38c63b02f3468778e6a83
PoriaEbrahimi <poria.ebrahimi123@gmail.com>
2022-05-21 05:07:36 -0500

    Updated footer components

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,21 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
-  socialMediaArray: String[] = [
-    "https://github.com/PoriaEbrahimi",
-    "mailto:poria.ebrahimi123@gmail.com",
-    "https://www.linkedin.com/in/poria-ebrahimi/"
-  ];
-
+  
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  randomSocialMedia = () => {
-    var pos = Math.floor(Math.random() * 3);
-    return this.socialMediaArray[pos]
   }
 
 }
