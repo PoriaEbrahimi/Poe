4558d438795d4fe6a59af5c2551b26ed7d51c1cd
PoriaEbrahimi <poria.ebrahimi123@gmail.com>
2022-05-21 05:17:13 -0500

    Updated app and routing modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { HeaderModule } from './header/header.module'
+
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ContentComponent } from './content/content.component';
+import { HomeComponent } from './body/home/home.component';
+import { BrowseComponent } from './body/browse/browse.component';
 import { FooterComponent } from './footer/footer.component';
+import { PageNotFoundComponent } from './body/page-not-found/page-not-found.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    ContentComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    BrowseComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeaderModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
